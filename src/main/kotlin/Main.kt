c59188b63628cf8fbc46e0568a2a59aee9438451
Antonio Monteiro <antoniohbmonteiro@gmail.com>
2021-08-13 15:49:08 -0300

    Atualizado exemplo

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,10 +5,10 @@ fun main() {
     // [-27, 3, 55, 10, -50, 97, -33, 14, 70]
     // pode utilizar esses ou criar o seu proprio
 
-//    val vetor = arrayListOf(5, 9, 3, 19, 70, 8, 100, 2, 35, 27)
+    val vetor = arrayListOf(5, 9, 3, 19, 70, 8, 100, 2, 35, 27)
 //    val vetor = arrayListOf(4, 50, 44, 180, 32, 86, 500, 2, 12, 6, 98, 100)
-    val vetor = arrayListOf(-27, 3, 55, 10, -50, 97, -33, 14, 70)
+//    val vetor = arrayListOf(-27, 3, 55, 10, -50, 97, -33, 14, 70)
 
     // exemplo de exercicio
-    println(Exercicios().maiorValor(vetor))
+    println(Exercicios().imprimaVetor(vetor))
 }
